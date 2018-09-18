bdd7bd8458a80a8cd62ba0ede625ede66a741f2a
Yiwei Liu <lywei@dinpay.com>
2018-09-18 23:53:09 +0800

    initial commit

--- src/main/java/com/openmind/singleton/LazyInitSingleton.java
+++ src/main/java/com/openmind/singleton/LazyInitSingleton.java
@@ -1,5 +1,9 @@
 package com.openmind.singleton;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+
 public class LazyInitSingleton {
 
     private static volatile LazyInitSingleton INSTANCE;
@@ -17,5 +21,37 @@ public class LazyInitSingleton {
         }
 
         return INSTANCE;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Set<Object> set = ConcurrentHashMap.newKeySet();
+
+        int size = 1000;
+        CountDownLatch countDownLatch = new CountDownLatch(size);
+
+        for (int i = 0; i < size; i++) {
+            final int idx = i;
+            new Thread(() -> {
+                LazyInitSingleton tmp = LazyInitSingleton.getInstance();
+                set.add(tmp);
+                System.out.println("Thread-" + idx + tmp);
+                countDownLatch.countDown();
+            }).start();
+        }
+
+
+        countDownLatch.await();
+        System.out.printf("size of set: %s \n", set.size());
+
+//        LazyInitSingleton t1 = LazyInitSingleton.getInstance();
+//        LazyInitSingleton t2 = LazyInitSingleton.getInstance();
+//
+//        //vm -ea
+//        assert t1 == t2 : "t1 not equal t2";
+//
+//        LazyInitSingleton t3 = new LazyInitSingleton();
+//        assert t1 == t3 : "t1 not equal t3";
+
     }
 }
